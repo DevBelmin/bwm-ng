17fa9e8334e0fed2c7c44dabe700eef359aece46
Belmin <sabic@artech-is.com>
2018-10-09 20:52:00 +0200

    Added rental modul with rental related components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
+import { RentalModule } from './rental/rental.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header/header.component';
-import { RentalComponent } from './rental/rental.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RouterModule } from '@angular/router';
 import { AppRouterModule } from './app-router/app-router.module';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RentalComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRouterModule
+    AppRouterModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
