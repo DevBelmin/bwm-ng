d2e9bdb97741c6bd81ac3dd26d1a4f89a8739c67
Belmin <sabic@artech-is.com>
2018-10-12 23:44:44 +0200

    Added request body validation on auth route, added node modules on server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,12 @@
 const User = require('../models/user');
+const { validationResult } = require('express-validator/check');
 
 exports.auth = function (req, res) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
 
     const username = req.body.username;
     const email = req.body.email;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/auth');
+const { check } = require('express-validator/check');
 
-router.post('', userController.auth);
+router.post('', [
+    check('username').not().isEmpty().isString(),
+    check('email').not().isEmpty().isString(),
+    check('password').not().isEmpty().isString(),
+    check('passwordConfirmation').not().isEmpty().isString(),
+
+    check('username').isLength({ min: 4 }),
+    check('username').isLength({ max: 32 }),
+    check('password').isLength({ min: 4 }),
+    check('password').isLength({ max: 32 }),
+    
+    check('email').isEmail(),
+
+  ],
+  userController.auth);
 
 router.post('/register', userController.register);
 
